fdee65565965c7989cd0236b98fff3ea7647c21a
alinassarrr <aliinassar8@gmail.com>
2025-06-27 22:07:02 +0300

    Add database connection

---
+++ connection/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_host = "127.0.0.1";
+$db_user = "root";
+$db_name = "smart-flix";
+$db_pass = null;
+$db_port = 3307;
+
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
