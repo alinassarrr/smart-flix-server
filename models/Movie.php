c3084b3adf2159afc8e26b0bb1132facab738680
alinassarrr <aliinassar8@gmail.com>
2025-07-02 09:52:39 +0300

    Add newline at the end of Movie.php file for consistency with coding standards

--- models/Movie.php
+++ models/Movie.php
@@ -95,4 +95,5 @@ class Movie extends Model
     {
         return ["id" => $this->id, "title" => $this->title, "description" => $this->description, "cover_image" => $this->cover_image, "trailer_url" => $this->trailer_url, "rating" => $this->rating, "release_date" => $this->release_date, "duration" => $this->duration];
     }
+
 }
