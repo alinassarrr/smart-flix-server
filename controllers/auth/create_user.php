a17a43a06425dd6817290a3e1cd4ff0db2c997e5
alinassarrr <aliinassar8@gmail.com>
2025-07-02 09:49:19 +0300

    Add CORS header to create_user.php for API access

--- controllers/auth/create_user.php
+++ controllers/auth/create_user.php
@@ -1,5 +1,5 @@
 <?php
-
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 require_once("../../connection/connection.php");
 require_once("../../models/User.php");
